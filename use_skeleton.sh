2ea3f05b0f028df463320c35ae9d8d1166906071
Jason Antman <jason@jasonantman.com>
2016-10-31 08:11:21 -0400

    fixes to use_skeleton

--- use_skeleton.sh
+++ use_skeleton.sh
@@ -17,31 +17,26 @@ srcdir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 projname=$1
 projname_underscored=${projname//-/_}
 
-mkdir -p $projname
-cd $projname
-
 function movefile {
     src=$1
     dst=$2
-    sed "s/python-package-skeleton/${projname}/g" ${srcdir}/${src} | sed "s/python_package_skeleton/${projname_underscored}/g" > $dst
+    sed "s/python-package-skeleton/${projname}/g" ${srcdir}/${src} | sed "s/python_package_skeleton/${projname_underscored}/g" > ${projname}/${dst}
 }
 
-mkdir -p .github docs/source
+mkdir -p $projname/.github $projname/docs/source/_static ${projname}/${projname_underscored}/tests
 
 for fname in .coveragerc .github/CONTRIBUTING.md .github/ISSUE_TEMPLATE.md .github/PULL_REQUEST_TEMPLATE.md .gitignore .landscape.yaml .travis.yml CHANGES.rst LICENSE MANIFEST.in pytest.ini setup.cfg setup.py tox.ini docs/Makefile docs/source/changes.rst docs/source/conf.py docs/source/index.rst docs/source/modules.rst
 do
     movefile $fname $fname
 done
 
-mkdir docs/source/_static
-touch docs/source/_static/.gitkeep
+touch $projname/docs/source/_static/.gitkeep
 
 sed "s/python-package-skeleton/${projname}/g" ${srcdir}/README_skeleton.rst > README.rst
 
-mkdir -p ${projname}/tests
 for fname in __init__.py version.py tests/__init__.py tests/test_version.py
 do
-    movefile python-package-skeleton/${fname} ${projname}/${fname}
+    movefile python_package_skeleton/${fname} ${projname_underscored}/${fname}
 done
 
 echo "Done."
